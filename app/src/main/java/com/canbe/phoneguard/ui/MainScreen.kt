43e81639b3002820946ffe17eba4b00d0a4e332b
HYUNJUNE <june2ac4dev@gmail.com>
2025-06-12 17:24:42 +0900

    Feat(UI):MainScreen Compose

--- app/src/main/java/com/canbe/phoneguard/ui/MainScreen.kt
+++ app/src/main/java/com/canbe/phoneguard/ui/MainScreen.kt
@@ -1,6 +1,10 @@
 package com.canbe.phoneguard.ui
 
 
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -13,19 +17,62 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.canbe.phoneguard.ui.theme.PhoneGuardTheme
+import timber.log.Timber
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun MainScreen(
     viewModel: MainViewModel = hiltViewModel(),
     onNavigateToSetting: () -> Unit
 ) {
+    MainScreenContent(onNavigateToSetting = onNavigateToSetting)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreenContent(
+    onNavigateToSetting: () -> Unit
+) {
+    val context = LocalContext.current
+
+    var isPermissionGranted by remember { mutableStateOf(false) }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        isPermissionGranted = isGranted
+        if (isGranted) {
+            Timber.e("testLog:1")
+        }
+    }
+
+    //최초 1회만 권한 확인
+    LaunchedEffect(Unit) {
+        val permission = Manifest.permission.READ_CONTACTS
+
+        isPermissionGranted = ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+
+        if (!isPermissionGranted) {
+            permissionLauncher.launch(permission)
+        } else {
+            //viewModel.loadContacts(context)
+            Timber.e("testLog:2")
+        }
+    }
+
     //UI
     Scaffold(
         topBar = {
@@ -39,7 +86,9 @@ fun MainScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                Text("This is the Main Screen")
+                val tt = if (isPermissionGranted) "TRUE" else "FALSE"
+
+                Text("This is the Main Screen $tt")
                 Spacer(modifier = Modifier.height(16.dp))
 
                 Button(onClick = onNavigateToSetting) {
@@ -54,6 +103,6 @@ fun MainScreen(
 @Composable
 fun MainScreenPreview() {
     PhoneGuardTheme {
-        MainScreen(onNavigateToSetting = {})
+        MainScreenContent(onNavigateToSetting = {})
     }
 }
